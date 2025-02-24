314c96c3b18022ea41e342a71654f0cf30492c28
ShimaaSalah334 <shimaaasalah2002@gmail.com>
2025-02-25 01:13:19 +0200

    22th

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { IProduct } from '../interfaces/iproduct';
 
 @Injectable({
@@ -19,5 +19,12 @@ export class ProductsService {
   }
   getProductsByCategory(category: string): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(`https://fakestoreapi.com/products/category/${category}`);
+  }
+  getProductIds(): Observable<string[]> {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => // Explicitly define the type of 'products'
+        products.map((product) => product.id.toString())
+      ),
+    );
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { getPrerenderParams } from '../prerender-params';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -36,7 +37,22 @@ app.use(
     redirect: false,
   }),
 );
+app.get('/details/:id', async (req, res, next) => {
+  const params = await getPrerenderParams(); // Fetch the list of IDs
+  const id = req.params.id;
 
+  // Check if the ID is in the list of prerendered IDs
+  if (params.some((param) => param.id === id)) {
+    angularApp
+      .handle(req)
+      .then((response) =>
+        response ? writeResponseToNodeResponse(response, res) : next(),
+      )
+      .catch(next);
+  } else {
+    next(); // Skip prerendering for unknown IDs
+  }
+});
 /**
  * Handle all other requests by rendering the Angular application.
  */
