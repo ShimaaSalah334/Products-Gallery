41626d5f6f9b141da99ccdef76106caa9620bf3c
ShimaaSalah334 <shimaaasalah2002@gmail.com>
2025-02-25 00:45:08 +0200

    17th

--- prerender-params.js
+++ prerender-params.js
@@ -2,12 +2,10 @@ const axios = require("axios");
 
 module.exports = async function getPrerenderParams() {
   try {
-    // Fetch products from your API
     const response = await axios.get("https://fakestoreapi.com/products");
     const products = response.data;
 
-    // Map product IDs for prerendering
-    return products.map((product) => ({ id: String(product.id) }));
+    return products.map((product) => ({ id: product.id.toString() }));
   } catch (error) {
     console.error("Error fetching product IDs for prerendering:", error);
     return [];
