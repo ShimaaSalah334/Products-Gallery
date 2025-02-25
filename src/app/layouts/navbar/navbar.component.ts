28ac9c3f52805b2e42ec96c4131b259510afe2a8
ShimaaSalah334 <shimaaasalah2002@gmail.com>
2025-02-25 19:42:28 +0200

    34th

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlowbiteService } from '../../core/services/flowbite.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +8,18 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   searchTerm: string = '';
 
   @Output() searchTermChange: EventEmitter<string> = new EventEmitter();
+  constructor(private flowbiteService: FlowbiteService,) { }
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
   onSearchChange() {
     this.searchTermChange.emit(this.searchTerm);
   }
